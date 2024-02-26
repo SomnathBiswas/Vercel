3275ff9e06be44e23e8b96dc67760b9aa0e3709e
Somnath Biswas <somnathbiswas576@gmail.com>
2024-02-26 16:29:00 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,28 +21,28 @@ module.exports = {
         'spin-slow': 'spin 8s linear infinite',
       },
       backgroundImage: {
-        circularLight:
+        "circularLight":
           "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px);",
 
-        circularDark:
+        "circularDark":
           "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 100px);",
 
-        circularLightLg:
+        "circularLightLg":
           "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 80px);",
 
-        circularDarkLg:
+        "circularDarkLg":
           "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 80px);",
 
-        circularLightMd:
+        "circularLightMd":
           "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 60px);",
 
-        circularDarkMd:
+        "circularDarkMd":
           "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 6px,#1b1b1b 60px);",
 
-        circularLightSm:
+        "circularLightSm":
           "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 40px);",
 
-        circularDarkSm:
+        "circularDarkSm":
           "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 4px,#1b1b1b 40px);",
       }
     },
