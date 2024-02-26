60dc7d9100c4e70ad6cd6e022b468699a6f720f3
Somnath Biswas <somnathbiswas576@gmail.com>
2024-02-26 18:38:52 +0530

    first commit

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -9,8 +9,8 @@ const HireMe = () => {
       <div className='w-48 h-auto flex items-center justify-center relative'>
         <CircularText className={"fill-dark animate-spin-slow dark:fill-light md:w-24"} />
       </div>
-      <Link href="https://docs.google.com/forms/d/e/1FAIpQLSc-RuJEWivw6P0dxOCEirNMOf-613QwxB10Me3eVqmJK8jYjg/viewform" target={"_blank"} className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full
-    font-semibold hover:bg-light hover:text-dark hover:border-dark hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark
+      <Link href="https://docs.google.com/forms/d/e/1FAIpQLSc-RuJEWivw6P0dxOCEirNMOf-613QwxB10Me3eVqmJK8jYjg/viewform" target={"_blank"} className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border-dark w-20 h-20 rounded-full
+    font-semibold hover:bg-light hover:text-dark hover:border-dark border-2 border-solid border-transparent
     dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:w-12 md:h-12 md:text-[10px]
     '>Hire Me</Link>
     </div>
